5efccbc407e60f3d169d52fb0421cf0343b5d533
Aluno IFSC <IFSC@CDR-770656.cacador.ifsc.edu.br>
2021-11-23 15:32:07 -0300

    instanciação dos objetos

---
+++ src/POO/Pessoa.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POO;
+
+/**
+ *
+ * @author IFSC
+ */
+public class Pessoa {
+    
+    String nome;
+    String corDaPele;
+    String altura;
+    String raça;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCorDaPele() {
+        return corDaPele;
+    }
+
+    public void setCorDaPele(String corDaPele) {
+        this.corDaPele = corDaPele;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getRaça() {
+        return raça;
+    }
+
+    public void setRaça(String raça) {
+        this.raça = raça;
+    }
+     
+    
+   
+    
+   
+        
+        
+    }
+    
+    
+    
+    
+    
+
